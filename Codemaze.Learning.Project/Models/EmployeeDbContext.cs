b4d06cda8386c2418ae7df2830a5322e8367b321
Mohsin Azam <mohsin.azam@maqta.ae>
2023-01-19 08:46:40 +0400

    Db Chnages

---
+++ Codemaze.Learning.Project/Models/EmployeeDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codemaze.Learning.Project.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        private readonly string connectionString;
+        private readonly ILoggerFactory loggerFactory = LoggerFactory.Create(config => config.AddConsole());
+        public EmployeeDbContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(loggerFactory);
+            optionsBuilder.UseSqlServer(connectionString);            
+        }
+    }
+}
--- Codemaze.Learning.Project/Program.cs
+++ Codemaze.Learning.Project/Program.cs
@@ -1,2 +1,47 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using Codemaze.Learning.Project.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+
+
+var context = new EmployeeDbContext("Data Source=AEADLT19726;Initial Catalog=Learning;User ID=sa;Password=xxx");
+// IEnumerable<Employee> employees = context.Employees.Where(e => e.Id > 1);
+IQueryable<Employee> employees = context.Employees.Where(e => e.Id > 1);
+//var topEmployees = employees.Take(2);
+
+foreach (var emp in employees)
+{ 
+     Console.WriteLine($"Name -   {emp.FirstName} {emp.LastName} has Address  { emp.Address}");
+}
+
+// Bulk Insert into Database.
+// using var connection_01 = new SqlConnection(Environment.GetEnvironmentVariable("Connection"));
+/*
+using var connection = new SqlConnection("Data Source=AEADLT19726;Initial Catalog=Learning;User ID=sa;Password=xxx");
+
+var table = new DataTable();
+table.Columns.Add("Id");
+table.Columns.Add("Name");
+table.Columns.Add("Description");
+table.Columns.Add("Type");
+
+for (int count = 0; count < 100; count++)
+{
+    table.Rows.Add(new object[] 
+    {
+        count + 1 ,
+        "Name" + count,
+        "Description" + count,
+        "Type" + count 
+    });
+}
+
+
+using var bulkCopy = new SqlBulkCopy(connection);
+bulkCopy.DestinationTableName = "Product";
+bulkCopy.BatchSize = 100;
+bulkCopy.BulkCopyTimeout = 60;
+connection.Open();
+bulkCopy.WriteToServer(table);
+*/
